f4155f6912017754432b66371229a851c972eefc
Oleksandr Buhaiov <oleksandr_buhaiov@epam.com>
2024-01-21 19:27:06 +0200

    Update handler to keep response field order stable

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @LambdaHandler(
@@ -15,7 +16,7 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
     public Map<String, Object> handleRequest(Object request, Context context) {
         System.out.println("Hello from lambda");
-        Map<String, Object> resultMap = new HashMap<String, Object>();
+        Map<String, Object> resultMap = new LinkedHashMap<>();
         resultMap.put("statusCode", 200);
         resultMap.put("body", "Hello from Lambda");
         return resultMap;
